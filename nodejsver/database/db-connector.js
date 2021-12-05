ca281d24d95b04123de0c5c8dfff14198367506d
Jianbo-Ning <81943889+Jianbo-Ning@users.noreply.github.com>
2021-12-05 13:01:24 -0800

    Add files via upload

--- nodejsver/database/db-connector.js
+++ nodejsver/database/db-connector.js
@@ -2,20 +2,23 @@
 // From nodejs-starter-app
 
 // Get an instance of mysql we can use in the app
-var mysql = require('mysql')
+var mysql = require('mysql2')
 
 // Create a 'connection pool' using the provided credentials
-var pool = mysql.createPool({
-    connectionLimit : 10,
-    host            : 'classmysql.engr.oregonstate.edu',
+ var pool = mysql.createPool({
+   connectionLimit : 10,
+   host            : 'classmysql.engr.oregonstate.edu',
     user            : 'cs340_evansdr',
     password        : '2582',
-    database        : 'cs340_evansdr'
-    // host            : 'localhost',
-    // user            : 'root',
-    // password        : 'root',
-    // database        : 'cs340_evansdr'
-})
+     database        : 'cs340_evansdr'
+ })
+/* var pool = mysql.createPool({
+    connectionLimit : 10,
+    host            : '127.0.0.1',
+    user            : 'root',
+    password        : 'root',
+    database        : 'Group106'
+})*/
 
 // Export it for use in our applicaiton
 module.exports.pool = pool;
